fde9beab4d157268f96507733efb0c383fee1a2e
Arthur Vinicius Ribeiro Silva <arthur.ribeiro_badaro@prestador.globo>
2023-11-06 08:57:33 -0300

    refactor(AuthController): adiciona permissão de cors

--- src/main/java/ufrn/tads/br/paii/controller/AuthController.java
+++ src/main/java/ufrn/tads/br/paii/controller/AuthController.java
@@ -4,15 +4,13 @@ import jakarta.validation.Valid;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ufrn.tads.br.paii.config.LoginDTO;
 import ufrn.tads.br.paii.config.TokenResponse;
 import ufrn.tads.br.paii.service.TokenService;
 
 @RestController
+@CrossOrigin(origins = "*")
 @RequestMapping("/login")
 public class AuthController {
 
